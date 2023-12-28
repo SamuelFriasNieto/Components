ab3de371c02825f1a9acfd387c80f25b1ce1e52c
SamuelFriasNieto <sae.nieto@gmail.com>
2023-12-28 14:07:06 +0100

    Comments on JavaScript

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -1,10 +1,10 @@
 const panels = document.querySelectorAll('.panel');
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        panels.forEach(panel => {
+panels.forEach(panel => { // Iterate on each panel
+    panel.addEventListener('click', () => { // When a panel is clicked, do something
+        panels.forEach(panel => { // Remove the class 'active' on all panels
             panel.classList.remove('active')
         })
-        panel.classList.add('active')
+        panel.classList.add('active')// Add the class 'active' on the clicked panel
     })
 })
